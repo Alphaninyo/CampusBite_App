22ef4bcd333e58a5c866d656b628287b09f9db8b
Alphaninyo <chamba.alpha@gmail.com>
2026-07-21 15:15:35 +0300

    Revert temporary verbose error message on menu image upload
    Cloudinary env vars are confirmed working now, no need to leak
    internal error details to API clients anymore.

--- CampusBite_Backend-main/src/controllers/menu.controller.js
+++ CampusBite_Backend-main/src/controllers/menu.controller.js
@@ -138,7 +138,7 @@ exports.updateMenuItem = async (req, res) => {
     res.status(200).json({ success: true, message: 'Menu item updated.', item });
   } catch (error) {
     console.error('[MENU] updateMenuItem error:', error);
-    res.status(500).json({ success: false, message: `Server error: ${error.message}` });
+    res.status(500).json({ success: false, message: 'Server error.' });
   }
 };
 
